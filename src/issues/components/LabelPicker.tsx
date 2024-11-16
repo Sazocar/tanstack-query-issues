6fada51337625a1b47bc80298713dbe2128456c0
Simón A. Azócar <saimn.azocar@gmail.com>
2024-11-16 18:37:44 -0400

    initialDate + placeholderData

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,26 +1,23 @@
-import { LoadingSpinner } from "../../shared";
-import { useLabels } from "../hooks";
+import { LoadingSpinner } from '../../shared';
+import { useLabels } from '../hooks';
 
 export const LabelPicker = () => {
-
   const { labelsQuery } = useLabels();
 
   if (labelsQuery.isLoading) {
-    return(
-      <LoadingSpinner />
-    )
+    return <LoadingSpinner />;
   }
 
   return (
     <div className="flex flex-wrap gap-2 justify-center">
-      {labelsQuery?.data?.map(({id, color, name}) => (
+      {labelsQuery?.data?.map(({ id, color, name }) => (
         <span
-        key={id}
-        className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
-        style={{ border: `1px solid #${color}` }}
-      >
-        {name}
-      </span>
+          key={id}
+          className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+          style={{ border: `1px solid #${color}` }}
+        >
+          {name}
+        </span>
       ))}
     </div>
   );
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,14 +1,54 @@
-import { useQuery } from "@tanstack/react-query";
-import { getLabels } from "../actions";
+import { useQuery } from '@tanstack/react-query';
+import { getLabels } from '../actions';
 
 export const useLabels = () => {
   const labelsQuery = useQuery({
     queryKey: ['labels'],
     queryFn: getLabels,
-    staleTime: 1000 * 60 * 60 // 1hr de staletime
-  })
+    staleTime: 1000 * 60 * 60, // 1hr de staletime
+    placeholderData: [
+      {
+        id: 791921801,
+        node_id: 'MDU6TGFiZWw3OTE5MjE4MDE=',
+        url: 'https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F',
+        name: '❤️',
+        color: 'ffffff',
+        default: false,
+        description: null,
+      },
+      {
+        id: 69105383,
+        node_id: 'MDU6TGFiZWw2OTEwNTM4Mw==',
+        url: 'https://api.github.com/repos/facebook/react/labels/Browser:%20IE',
+        name: 'Browser: IE',
+        color: 'c7def8',
+        default: false,
+        description: null,
+      },
+    ],
+    // initialData: [
+    //   {
+    //     id: 791921801,
+    //     node_id: 'MDU6TGFiZWw3OTE5MjE4MDE=',
+    //     url: 'https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F',
+    //     name: '❤️',
+    //     color: 'ffffff',
+    //     default: false,
+    //     description: null,
+    //   },
+    //   {
+    //     id: 69105383,
+    //     node_id: 'MDU6TGFiZWw2OTEwNTM4Mw==',
+    //     url: 'https://api.github.com/repos/facebook/react/labels/Browser:%20IE',
+    //     name: 'Browser: IE',
+    //     color: 'c7def8',
+    //     default: false,
+    //     description: null,
+    //   },
+    // ],
+  });
 
   return {
-    labelsQuery
-  }
-}
+    labelsQuery,
+  };
+};
