cbd205c38988bf40cf4020b919f7b1270da48a70
Simón A. Azócar <saimn.azocar@gmail.com>
2024-11-16 20:03:04 -0400

    cargar comentarios del issue

---
+++ src/issues/actions/get-issue-comment.ts
@@ -0,0 +1,10 @@
+import { sleep } from "../../helpers";
+import { githubApi } from "../../api/github.api";
+import { GithubIssue } from "../interfaces";
+
+export const getIssueComments = async (issueNumber: number): Promise<GithubIssue[]> => {
+  await sleep(1500);
+
+  const { data } = await githubApi.get<GithubIssue[]>(`/issues/${issueNumber}/comments`)
+  return data;
+}
--- src/issues/actions/get-issue.ts
+++ src/issues/actions/get-issue.ts
@@ -2,9 +2,9 @@ import { sleep } from "../../helpers";
 import { githubApi } from "../../api/github.api";
 import { GithubIssue } from "../interfaces";
 
-export const getIssue = async (isseuNumber: number): Promise<GithubIssue> => {
+export const getIssue = async (issueNumber: number): Promise<GithubIssue> => {
   await sleep(1500);
 
-  const { data } = await githubApi.get<GithubIssue>(`/issues/${isseuNumber}`)
+  const { data } = await githubApi.get<GithubIssue>(`/issues/${issueNumber}`)
   return data;
 }
